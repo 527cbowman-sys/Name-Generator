31e6ac49c6e75b42bf08e9f97f2cdf9ab70cf912
527cbowman-sys <527cbowman@frhsd.com>
2026-01-08 09:31:37 -0500

    1

---
+++ script.js
@@ -0,0 +1,46 @@
+if (score > 2.5) {
+    alert('Ho ho ho! On the nice list you go!')
+} else {
+    alert('You\'re on the naughty list!')
+}
+
+
+
+switch(randomNumber){
+        case 0:
+            image = 'yes.png'
+            color = 'text-success';
+            break;
+        case 1:
+            image = 'no.png'
+            color = 'text-danger';
+            break;
+        case 2:
+            image = 'maybe.png'
+            color = 'text-warning';
+            break;
+        case 3:
+            image = 'notlikely.png'
+            color = 'text-warning';
+            break;
+        case 4:
+            image = 'doubtful.png'
+            color = 'text-danger';
+            break;
+        case 5:
+            image = 'cannotpredict.png'
+            color = 'text-warning';
+            break;
+        case 6:
+            image = 'askagainlater.png'
+            color = 'text-muted';
+            break;
+        case 7:
+            image = 'mostlikely.png'
+            color = 'text-success';
+            break;
+        case 8:
+            image = 'certainly.png'
+            color = 'text-success';
+            break;
+    }
